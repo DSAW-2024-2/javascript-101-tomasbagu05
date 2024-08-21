d6c0fa9695f9827b2fda291a091ab36a104421aa
Tomas <tomasbarriosguevara@gmail.com>
2024-08-21 11:48:34 -0500

    Operaciones

--- solutions.js
+++ solutions.js
@@ -1,27 +1,70 @@
 // Sum of Two Numbers
 function sum(a, b) {
-  // Add your code here
+  if(typeof a === 'number' && typeof b === 'number') {
+    return a +b;
+  }
+  else{
+    console.log("Los datos de entrada no son adecuados");
+  }
 }
 
 // Factorial of a Number
 function factorial(n) {
-  // Add your code here
+  if (typeof n === 'number') {
+    if (n === 0 || n === 1) {
+      return 1;
+    } else {   
+      return n*factorial(n-1);
+    } 
+  }
+  else {
+    console.log("Los datos de entrada no son adecuados");
+  }
 }
 
 // Find the Largest Number
 function findLargest(arr) {
-  // Add your code here
+  if (Array.isArray(arr) && arr.length > 0) {
+    if (arr.every(numero => typeof numero === 'number')) {
+      let Max_numero = Math.max(...arr);
+      return Max_numero;
+    } 
+  } else {
+    console.log("Los datos de entrada no son adecuados");
+    return null;
+  }
 }
+
 
 // Count Vowels in a String
 function countVowels(str) {
-  // Add your code here
+  if (typeof str !== 'string') {
+    console.log("Los datos de la entrada no son adecuadas");
+    return null;
+  }
+  const vowels = 'aeiou';
+  let count = 0;
+  for (let char of str) {
+    if (vowels.includes(char.toLowerCase())) {
+      count++;
+    }
+  } return count;
 }
 
+     
+
 // Check if a Number is Prime
-function isPrime(n) {
-  // Add your code here
-}
+  function isPrime(n) {
+    if (n <= 1) {
+      return false;
+    }
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
 
 module.exports = {
   sum,
